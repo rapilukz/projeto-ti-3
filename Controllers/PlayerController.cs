24cc4a82c5a88d7b19ff2d4a084ac1f9941583ee
Gabriel Gomes <gabrielramosgomes82@gmail.com>
2024-06-14 01:30:47 +0100

    Added player delete, finally

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using System.Net;
 
 namespace Maio11_Best.Controllers
 {
@@ -36,23 +37,73 @@ namespace Maio11_Best.Controllers
         [HttpPost]
         public ActionResult InsertPlayer(player newPlayer, HttpPostedFileBase fich)
         {
-            
+
             using (DbModel db = new DbModel())
             {
                 db.players.Add(newPlayer);
                 db.SaveChanges();
-                if(fich != null && fich.FileName.Length > 0 && fich.ContentType.Contains("image"))
+                if (fich != null && fich.FileName.Length > 0 && fich.ContentType.Contains("image"))
                 {
                     string path = Server.MapPath("~/fotos/players/");
                     string file = newPlayer.player_id.ToString() + System.IO.Path.GetExtension(fich.FileName);
                     newPlayer.photo_path = file;
                     path += file;
                     fich.SaveAs(path);
-                    db.SaveChanges();   
+                    db.SaveChanges();
                 }
-                return RedirectToAction("PlayerList", new { msg="Inserido com sucesso" });
+                return RedirectToAction("PlayerList", new { msg = "Inserido com sucesso" });
             }
 
+        }
+
+        public ActionResult DeletePlayer(int? id)
+        {
+            using (DbModel db = new DbModel())
+            {
+                player deletedPlayer = db.players
+                                         .Include(p => p.Team)
+                                         .FirstOrDefault(p => p.player_id == (id ?? -1));
+                if (deletedPlayer != null)
+                {
+                    return View(deletedPlayer);
+                }
+                else
+                {
+                    return RedirectToAction("ListaCarros", new { msg = "Registo não existe" });
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DelPlayer(int player_id)
+        {
+            using (DbModel db = new DbModel())
+            {
+                try
+                {
+                    // Find the player by player_id
+                    player playerToDelete = db.players.Find(player_id);
+
+                    if (playerToDelete == null)
+                    {
+                        return HttpNotFound();
+                    }
+
+                    // Remove the player from the context
+                    db.players.Remove(playerToDelete);
+                    db.SaveChanges();
+
+                    // Redirect to PlayerList with a success message
+                    return RedirectToAction("PlayerList");
+                }
+                catch (Exception ex)
+                {
+                    // Handle any errors that occur during deletion
+                    ViewBag.ErrorMsg = "Error deleting player: " + ex.Message;
+                    return View(); // Optionally, you can return a view with an error message
+                }
+            }
         }
     }
 }
